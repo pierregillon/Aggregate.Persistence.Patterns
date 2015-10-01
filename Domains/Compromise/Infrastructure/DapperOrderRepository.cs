4e0b22a965fba6c1effe85aebddfc9a7671f74a1
Pierre GILLON <gillon.pierre69@gmail.com>
2015-10-01 23:36:33 +0200

    Add DapperOrderRepository for compromise pattern.

---
+++ Domains/Compromise/Infrastructure/DapperOrderRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using Domains.Compromise.Domain;
+
+namespace Domains.Compromise.Infrastructure
+{
+    public class DapperOrderRepository : IOrderRepository
+    {
+        public Order Get(Guid id)
+        {
+            using (var connection = new SqlConnection(@"Server=localhost\\SQLEXPRESS;database=DomainModelPatterns.Compromise;")) {
+                return connection.Query<Order>("SELECT * FROM Order WHERE Id = @id", new {id}).FirstOrDefault();
+            }
+        }
+        public void Add(Order order)
+        {
+            using (var connection = new SqlConnection(@"Server=localhost\\SQLEXPRESS;database=DomainModelPatterns.Compromise;")) {
+                connection.Execute(@"INSERT INTO Order (Id, OrderStatus, TotalCost, SubmitDate) VALUES(@a, @b, @c, @d)", new
+                {
+                    a = order.Id,
+                    b = order.OrderStatus,
+                    c = order.TotalCost,
+                    d = order.SubmitDate
+                });
+            }
+        }
+    }
+}
